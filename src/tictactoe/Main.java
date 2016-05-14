7da76eb6f5cbe619ae8f512fcb1c5bdf2e8b8c24
Igor Gayvan <igor.gayvan@gmail.com>
2016-05-14 09:41:58 +0300

    Add method showTable

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -15,7 +15,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        TicTacToe ticTacToe = new TicTacToe();
+        
+        ticTacToe.showTable();
+//        ticTacToe.
     }
-    
+
 }
--- src/tictactoe/ticTacToe.java
+++ src/tictactoe/ticTacToe.java
@@ -5,10 +5,37 @@
  */
 package tictactoe;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Igor Gayvan
  */
-public class ticTacToe {
-    
+public class TicTacToe {
+
+    private char[][] table;
+
+    public void showTable() {
+        System.out.print("  ");
+        for (int i = 0; i < table[0].length; i++) {
+            System.out.print("" + i);
+        }
+        System.out.println("");
+        
+        for (int i = 0; i < table.length; i++) {
+            System.out.print(i + "|");
+
+            for (int j = 0; j < table[i].length; j++) {
+
+                if (table[i][j] != 0) {
+                    System.out.print("" + table[i][j]);
+                } else {
+                    System.out.print("_");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+
 }
