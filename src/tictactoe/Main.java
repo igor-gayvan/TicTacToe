80735c43b09423ffc31ed5d01fcdf7d0157d8864
Igor Gayvan <igor.gayvan@gmail.com>
2016-05-14 09:46:08 +0300

    Добавили конструктор для создания массива нужной размерности

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -16,8 +16,8 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        TicTacToe ticTacToe = new TicTacToe();
-        
+        TicTacToe ticTacToe = new TicTacToe(3, 3);
+
         ticTacToe.showTable();
 //        ticTacToe.
     }
--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -13,7 +13,22 @@ import java.util.Arrays;
  */
 public class TicTacToe {
 
+    private int x = 3; //размер поля по горизонтали
+    private int y = 3; //размер поля по вертикали	
+
     private char[][] table;
+
+    public TicTacToe(int x, int y) {
+
+        if (x > 3) {
+            this.x = x;
+        }
+        if (y > 3) {
+            this.y = y;
+        }
+
+        table = new char[this.y][this.x];
+    }
 
     public void showTable() {
         System.out.print("  ");
@@ -21,7 +36,7 @@ public class TicTacToe {
             System.out.print("" + i);
         }
         System.out.println("");
-        
+
         for (int i = 0; i < table.length; i++) {
             System.out.print(i + "|");
 
