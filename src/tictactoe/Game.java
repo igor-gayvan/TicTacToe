3b125dab7bbbfa3c81e7bf6dd088904c56544c60
Igor Gayvan <igor.gayvan@gmail.com>
2016-05-14 11:29:20 +0300

    В цикле до окончания игры получаем ходы игроков, проверяем их на возмодность, проверяем признак условие окончания игры

--- src/tictactoe/Game.java
+++ src/tictactoe/Game.java
@@ -18,15 +18,14 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        TicTacToe ticTacToe;
+        TicTacToe ticTacToe = null;
 
 //        ticTacToe.showPlayerMessage();
         Scanner sc = null;
 
-        int x = 0;
-        int y = 0;
+        int cntCellX = 0;
+        int cntCellY = 0;
 
-        
         boolean isInitTable = false;
 
         while (true) {
@@ -36,27 +35,68 @@ public class Game {
 
                     sc = new Scanner(System.in);
 
-                    if (x == 0) {
+                    if (cntCellX == 0) {
                         System.out.print("Введите количество клеток (целое число) по X (минимальный размер 3): ");
-                        x = sc.nextInt();
+                        cntCellX = sc.nextInt();
                     }
 
-                    if (y == 0) {
+                    if (cntCellY == 0) {
                         System.out.print("Введите количество клеток (целое число) по Y (минимальный размер 3): ");
-                        y = sc.nextInt();
+                        cntCellY = sc.nextInt();
                     }
 
-                    ticTacToe = new TicTacToe(x, y);
+                    ticTacToe = new TicTacToe(cntCellX, cntCellY);
                     ticTacToe.showTable();
-                    
+
                     isInitTable = true;
-                    //obj.getUserMessage(); 
+                    //ticTacToe.showPlayerMessage(); 
                 } catch (InputMismatchException e) {
                     System.out.println("Введены не верные символы");
                 }
             } else {
                 // в бесконечном цикле получаем ходы игроков
-                
+                String str = sc.nextLine();
+                if (!str.equals("")) {
+                    String[] position = str.split(",");
+
+                    if (position.length == 2) {
+
+                        try {
+                            int moveX = Integer.parseInt(position[0]);
+                            int moveY = Integer.parseInt(position[1]);
+
+                            ticTacToe.setMovePosition(moveX, moveY);
+
+                            int statusCell = ticTacToe.checkCell(moveX, moveY);
+
+                            // если ход возможен и ячейка не занятеа
+                            if (statusCell > 0) {
+
+                                boolean isGameOver = ticTacToe.checkGameOver();
+                                String message = "Игра окончена!\n" + "Победа игрока №" + ticTacToe.currentPlayer;
+
+                                ticTacToe.makeMove();
+                                ticTacToe.showTable();
+
+                                if (isGameOver) {
+                                    System.out.println(message);
+                                    break;
+                                }
+
+                            } else if (statusCell == -1) {
+                                System.out.println("Выход за границы доски");
+                            } else {
+                                System.out.println("Указанная ячейка занята");
+                            }
+
+                        } catch (NumberFormatException e) {
+                            System.out.println("Координаты: должны быть целые числа!");
+                        }
+                    } else {
+                        System.out.println("Координаты должны быть в виде 'n,n)'!");
+                    }
+                }
+                ticTacToe.showPlayerMessage();
             }
 
         } // end while  
